e02e071e9f8dc725f1d51dcc0f87900234719658
rebekahyaumama <hippogrammer@gmail.com>
2020-03-24 02:28:40 -0400

    trying to fix deploy server

--- src/app/ngrx/app.effects.ts
+++ src/app/ngrx/app.effects.ts
@@ -7,14 +7,13 @@ import { of, EMPTY } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 import { AppState } from './app.reducer';
 import { getInbox } from './app.selectors';
-import { getSavedState, localStorageKey } from './storage.metaReducer';
 @Injectable()
 export class AppEffects {
 
   fetchEmails$ = createEffect(() => this.actions$.pipe(
     ofType(ActionTypes.FetchEmails),
     switchMap(() => {
-      let inbox = getSavedState(localStorageKey).appState.inbox;
+      let inbox = JSON.parse(localStorage.getItem('__app_storage__')).appState.inbox;
       if(Object.keys(inbox).length === 0){
         return this.service.getEmailsJSON().pipe(
           map(res =>  ({ type: ActionTypes.FetchEmailsSuccess, emails: res})),
