cadd59cd6a2e7060552858077eb5a151b7b75c67
rebekahyaumama <hippogrammer@gmail.com>
2020-03-24 02:29:10 -0400

    trying to fix deploy server error

--- src/app/ngrx/storage.metareducer.ts
+++ src/app/ngrx/storage.metareducer.ts
@@ -4,14 +4,14 @@ import {merge, pick} from 'lodash-es';
 function setSavedState(state: any, localStorageKey: string) {
   localStorage.setItem(localStorageKey, JSON.stringify(state));
 }
-export function getSavedState(localStorageKey: string): any {
+function getSavedState(localStorageKey: string): any {
   return JSON.parse(localStorage.getItem(localStorageKey));
 }
 
 // the keys from state which we'd like to save.
 const stateKeys = ['appState'];
 // the key for the local storage.
-export const localStorageKey = '__app_storage__';
+const localStorageKey = '__app_storage__';
 
 export function storageMetaReducer<S, A extends Action = Action> (reducer: ActionReducer<S, A>) {
   let onInit = true; // after load/refresh…
