1ee532d99a62cda997bc810a2a3c709cc544c899
rebekahyaumama <hippogrammer@gmail.com>
2020-03-24 18:39:18 -0400

    added metareducer into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Action, ActionReducer } from '@ngrx/store';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { appReducer } from './ngrx/app.reducer';
 import { AppEffects } from './ngrx/app.effects';
@@ -31,11 +31,42 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select'; 
-import { storageMetaReducer } from './ngrx/storage.metaReducer';
 import { MatCardModule } from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatChipsModule } from '@angular/material/chips';
+import {merge, pick} from 'lodash-es';
+
+function setSavedState(state: any, localStorageKey: string) {
+  localStorage.setItem(localStorageKey, JSON.stringify(state));
+}
+function getSavedState(localStorageKey: string): any {
+  return JSON.parse(localStorage.getItem(localStorageKey));
+}
+
+// the keys from state which we'd like to save.
+const stateKeys = ['appState'];
+// the key for the local storage.
+const localStorageKey = '__app_storage__';
+
+export function storageMetaReducer<S, A extends Action = Action> (reducer: ActionReducer<S, A>) {
+  let onInit = true; // after load/refresh…
+  return function(state: S, action: A): S {
+    // reduce the nextState.
+    const nextState = reducer(state, action);
+    // init the application state.
+    if (onInit) {
+      onInit           = false;
+      const savedState = getSavedState(localStorageKey);
+      return merge(nextState, savedState);
+    }
+    // save the next state to the application storage.
+    const stateToSave = pick(nextState, stateKeys);
+    setSavedState(stateToSave, localStorageKey);
+    return nextState;
+  };
+}
+
 @NgModule({
   declarations: [
     AppComponent,
